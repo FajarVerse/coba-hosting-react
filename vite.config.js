904d788e6be20bb6e8a6306aedf42e0dcdb8da7d
Gifarfajar321 <gifarifajarm174@gmail.com>
2024-10-25 12:56:09 +0700

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import  vercel from "vite-plugin-vercel"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/coba-hosting-react/",
+    server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [react()], [vercel()]
 });
